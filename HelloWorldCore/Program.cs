5f26897b72fc09ebed2a684d44af5ba9e7e8aa0a
Seiya_Satoh <satohseiya0312@gmail.com>
2021-08-05 18:23:16 +0900

    [update]テスト実行

--- HelloWorldCore/Program.cs
+++ HelloWorldCore/Program.cs
@@ -2,9 +2,9 @@
 
 namespace HelloWorldCore
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
             Console.WriteLine("Hello World!");
         }
